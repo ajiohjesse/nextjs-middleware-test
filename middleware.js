2285beb5e8c60c0becd58e906251698ff8f03c9a
ajiohjesse <ajiohjesse@gmail.com>
2022-11-23 10:46:48 +0100

    Add fetch to middleware

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(req) {
+  fetch('https://fakestoreapi.com/products/1')
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+
   return NextResponse.redirect(new URL('it-worked', req.nextUrl))
 }
 
