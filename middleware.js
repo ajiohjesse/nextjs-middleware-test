fab891a8216100b55654fa1466d9e92f383ef195
ajiohjesse <ajiohjesse@gmail.com>
2022-11-23 10:51:07 +0100

    remove fetch and matcher

--- middleware.js
+++ middleware.js
@@ -1,13 +1,9 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(req) {
-  fetch('https://fakestoreapi.com/products/1')
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-
   return NextResponse.redirect(new URL('it-worked', req.nextUrl))
 }
 
-export const config = {
-  matcher: '/dashboard',
-}
+// export const config = {
+//   matcher: '/dashboard',
+// }
